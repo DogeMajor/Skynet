01d1eb26aeb4026f9e16ca9c8f26865e27d06a29
Juho Kilkki <juho.kilkki@gmail.com>
2017-03-18 17:52:23 +0200

    Changes of variable names and added comments

--- bin/chunknet.py
+++ bin/chunknet.py
@@ -17,10 +17,17 @@ y = asarray([yo for yi, yo in obs]).reshape((BATCHSIZE, 1, 1))
 
 lrate = .2
 mom = .4
-reg = 1.e-10
+reg = 1.e-4
 # reg = 0.
 
 class ChunkNet(object):
+
+    """
+    Data attributes:
+    W       list of weight matrices
+    x       list of unactivated data vectors (vertical)
+    ax      list of activated data vectors (vertical)
+    """
 
     def __init__(self, hidden_sizes, dao=None):
         # Shape of net
@@ -31,81 +38,83 @@ class ChunkNet(object):
         if dao is not None:
             raise NotImplementedError()
         # Data arrays
-        self.x = [empty((BATCHSIZE, size, 1)) for size in self.form]
+        self.ax = [empty((BATCHSIZE, size, 1)) for size in self.form]
         # Data arrays but unactivated
-        self.s = [None] + [empty_like(xi) for xi in self.x[1:]]
-        # Change of data arrays
-        self.ds = [None] + [empty_like(xi) for xi in self.x[1:]]
+        self.x = [None] + [empty_like(xi) for xi in self.ax[1:]]
+        # Gradients of energy wrt. unactivated data at each layer
+        self.dEdx = [None] + [empty_like(xi) for xi in self.ax[1:]]
         # Uniform random for weight initialization
         unif_rand = lambda shape: 2*(2*rand(*shape) - 1)
         # Weights
-        self.w = [unif_rand((BATCHSIZE, self.form[i+1], self.form[i])) for i in range(self.depth-1)]
+        self.W = [unif_rand((BATCHSIZE, self.form[i+1], self.form[i])) for i in range(self.depth-1)]
         # Weight gradients
-        self.dw = [zeros_like(wi) for wi in self.w]
+        self.dW = [zeros_like(Wi) for Wi in self.W]
         # First data array equals input observations
-        self.x[0] = asarray([yi for yi, yo in obs]).reshape((BATCHSIZE, 2, 1))
+        self.ax[0] = asarray([yi for yi, yo in obs]).reshape((BATCHSIZE, 2, 1))
 
     def activate(self):
-        s, x, w = self.s, self.x, self.w
+        x, ax, W = self.x, self.ax, self.W
         for i in range(1, self.depth):
-            s[i][:] = einsum('dij,djk->dik', w[i-1], x[i-1])
-            x[i][:] = K(s[i])
+            x[i][:] = einsum('dij,djk->dik', W[i-1], ax[i-1])
+            ax[i][:] = K(x[i])
 
     def weight_gradient(self):
-        s, x, W, dEds, dEdW = self.s, self.x, self.w, self.ds, self.dw
 
-        # error = y - x[-1]
-        # ds[-1] = dK(s[-1])*error
-        # dEdW[-1][:] = ds[-1]*x[-2].swapaxes(-1, -2)
+        x, ax, W, dEdx, dEdW = self.x, self.ax, self.W, self.dEdx, self.dW
 
-        # Output layer is special
-        error = x[-1] - y
-        dEds[-1] = error*dK(s[-1])
-        dEdW[-1] = dEds[-1]*x[-2].swapaxes(-1, -2)
-        # Backpropagate gradients over the rest of the weight layers.
+        # Output layer first.
+        # Observation error
+        error = ax[-1] - y
+        # E for energy = cost function, d for derivative
+        dEdx[-1] = error*dK(x[-1])
+        dEdW[-1] = dEdx[-1]*ax[-2].swapaxes(-1, -2)
+
+        # The rest of the weight layers.
         # For 1 hidden layer, this loop is just i=0
         for i in range(self.depth-3, -1, -1):
-
-            # ds[i+1] = ds[i+2]*W[i+1].swapaxes(-1, -2)*dK(s[i+1])
-            # dEdW[i] = ds[i+1]*x[i].swapaxes(-1, -2)
-
-            dEds[i+1] = einsum('dnm,dni->di', dEds[i+2], W[i+1])[..., None]*dK(s[i+1])
-            dEdW[i] = dEds[i+1]*x[i].swapaxes(-1, -2)
+            # Gradient wrt. unactivated data.
+            # The sum corresponds to contributions by (unactivated) i+2 data layer.
+            # To match dimensions, shape of the sum expression is spread.
+            # Each activation gradient is multiplied elementwise.
+            dEdx[i+1] = einsum('dnm,dni->di', dEdx[i+2], W[i+1])[..., None]*dK(x[i+1])
+            # Gradient wrt. weight matrix
+            dEdW[i] = dEdx[i+1]*ax[i].swapaxes(-1, -2)
             # Regularization term gradient
-            dEdW[i] += W[i]**3*reg
+            dEdW[i] += W[i]**1*reg
+
         return dEdW
 
 class Descender(object):
 
     def __init__(self, net):
         self.net = net
-        self.delta_prev = [copy(dwi) for dwi in net.dw]
+        self.delta_prev = [copy(dWi) for dWi in net.dW]
 
     def descend_batch(self):
-        w = self.net.w
-        x = self.net.x
+        W = self.net.W
+        ax = self.net.ax
         # Activate net
         self.net.activate()
-        # Backpropagate gradients
+        # Energy gradient matrices
         dEdW = self.net.weight_gradient()
         # Update weights
         for wi in range(net.depth-1):
-            # Weight change. Doesn't include momentum
+            # Weight increment. Doesn't include momentum
             delta_wi = -lrate*dEdW[wi]
             # Descend step
-            w[wi][:] += delta_wi + mom*self.delta_prev[wi]
-            # Save weight change for next iteration momentum
+            W[wi][:] += delta_wi + mom*self.delta_prev[wi]
+            # Save weight increment for the next step
             self.delta_prev[wi][:] = delta_wi
         # Squared errors
-        sqe = (y - x[-1])**2
-        return sqe
+        sqerrors = (y - ax[-1])**2
+        return sqerrors
 
     def descend(self, N):
         for i in range(N):
             chunk_errors = [self.descend_batch() for _ in range(1)]
-            sqe = np.mean(chunk_errors)
-            print(i, sqe)
-            yield i, sqe
+            sqerror = np.mean(chunk_errors)
+            print(i, sqerror)
+            yield i, sqerror
 
 if __name__=='__main__':
 
